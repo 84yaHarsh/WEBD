775b63ff6b6dafa2060c82d5f8757103b1dd8617
84yaharsh <chauharsh12345@gmail.com>
2025-08-23 19:11:14 +0530

    web d practice

---
+++ JAVASCRIPT/PART-1/LINKING/app.js
@@ -0,0 +1,29 @@
+console.log("apna college");
+console.log("harsh chaurasia");
+let a = 5;
+let b= 6;
+console.log("sum is",a+b);
+// this is comment
+
+// tempelet Literal
+let samosha = 10;
+let lassi = 20;
+console.log(`total price is:${samosha+lassi} ruppes.`);
+// they are used to add embedded expression in a string
+
+
+// arithmetic operator
+let x = 6;
+let y = 4;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+
+// uuary operator(++,--)
+let c = 3;
+let d = 5;
+console.log(c++); // 3 first print then increase
+console.log(++c); // 5 first increase then print
