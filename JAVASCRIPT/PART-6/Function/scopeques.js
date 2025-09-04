32c6409e2e22efd13b5ef78601276b03b3925c5e
84yaharsh <chauharsh12345@gmail.com>
2025-09-04 23:55:56 +0530

    javaScript

---
+++ JAVASCRIPT/PART-6/Function/scopeques.js
@@ -0,0 +1,10 @@
+let greet = "hello";
+function changeGreet(){
+    let greet = "namaste";
+    console.log(greet);  // namaste
+    function innerGreet(){
+        console.log(greet);
+    }
+}
+console.log(greet); // hello
+changeGreet();
