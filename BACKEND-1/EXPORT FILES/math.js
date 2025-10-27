d8b165407c5c9d49a12d37a8cace5436b927ca5f
84yaharsh <chauharsh12345@gmail.com>
2025-10-27 20:54:59 +0530

    NodeJS

---
+++ BACKEND-1/EXPORT FILES/math.js
@@ -0,0 +1,13 @@
+const Sum = (a,b) => a+b;
+const diff = (a,b) =>a-b;
+const mul = (a,b) =>a*b;
+const PI= 3.14;
+
+module.exports = {
+   Sum : sum,
+   Subs : diff,
+   Mul : mul,
+   PI: PI,
+};
+
+/*math.exports.sum = (a,b) => a+b */
---
+++ BACKEND-1/EXPORT FILES/script.js
@@ -0,0 +1,6 @@
+let math = require("./math");
+console.log(math.Sum(5,4));
+console.log(math.Subs(7,2));
+console.log(math.Mul(4,2));
+console.log(math.PI);
+
